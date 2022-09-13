a29e7325ffbb673016a6a78aeefc9e02ce917f73
adarshangadi64 <adarsh@electems.com>
2022-09-13 12:38:23 +0530

    initial commit

---
+++ adarsh/reports/client/src/app/components/panel-detials/panel-detials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-detials',
+  templateUrl: './panel-detials.component.html',
+  styleUrls: ['./panel-detials.component.css']
+})
+export class PanelDetialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ adarsh/reports/client/src/app/components/panel-list/panel-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-list',
+  templateUrl: './panel-list.component.html',
+  styleUrls: ['./panel-list.component.css']
+})
+export class PanelListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
