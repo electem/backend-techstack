2321b71f86069a7a4f20f2dfc71a2c28d921f89c
shashikiran0620 <shashi@electems.com>
2022-09-14 18:24:53 +0530

    jest for report task

--- shashi/report/backend/test/utils/generate.ts
+++ shashi/report/backend/test/utils/generate.ts
@@ -1,157 +1,29 @@
 import faker from "faker";
-import { User, Post, Tutorial } from "../../src/models";
+import { Panel } from "../../src/models";
 
-export function generateUserData(overide = {}) {
+export function generatePanelData(overide = {}) {
   return {
     id: faker.random.number(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    email: faker.internet.email(),
-    posts: [],
-    comments: [],
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    ...overide,
-  };
-}
-
-export function generateUsersData(n: number = 1) {
-  return Array.from(
-    {
-      length: n,
-    },
-    (_, i) => {
-      return generateUserData();
-    }
-  );
-}
-
-export function generateUserPayload() {
-  return {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    email: faker.internet.email(),
-  };
-}
-
-export function generatePostData(overide = {}) {
-  return {
-    id: faker.random.number(),
-    title: faker.lorem.sentence(),
-    content: faker.lorem.paragraph(),
-    userId: faker.random.number(),
-    comments: [],
-    user: new User(),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    ...overide,
-  };
-}
-
-export function generatePostsData(n: number = 1, overide = {}) {
-  return Array.from(
-    {
-      length: n,
-    },
-    (_, i) => {
-      return generatePostData(overide);
-    }
-  );
-}
-
-export function generatePostPayload() {
-  return {
-    title: faker.lorem.sentence(),
-    content: faker.lorem.paragraph(),
-    userId: faker.random.number(),
-  };
-}
-
-export function generateCommentData(overide = {}) {
-  return {
-    id: faker.random.number(),
-    content: faker.lorem.paragraph(),
-    tutorialId: faker.random.number(),
-    //userId: faker.random.number(),
-    //user: new User(),
-    //postId: faker.random.number(),
-    // post: new Post(),
-    createdAt: new Date(),
-    //updatedAt: new Date(),
-    ...overide,
-  };
-}
-
-export function generateCommentsData(n: number = 1, overide = {}) {
-  return Array.from(
-    {
-      length: n,
-    },
-    (_, i) => {
-      return generateCommentData(overide);
-    }
-  );
-}
-
-export function generateCommentPayload() {
-  return {
-    content: faker.lorem.paragraph(),
-    tutorialId: faker.random.number(),
-    //postId: faker.random.number(),
-  };
-}
-
-//tutorial
-
-export function generateTutorialData(overide = {}) {
-  return {
-    id: faker.random.number(),
-    title: faker.name.title(),
+    name: faker.name.title(),
     description: faker.lorem.text(),
+    tests: [],
     ...overide,
   };
 }
-
-export function generateTutorialsData(n: number = 1, overide = {}) {
+export function generatePanelsData(n: number = 1) {
   return Array.from(
     {
       length: n,
     },
     (_, i) => {
-      return generateTutorialData(overide);
+      return generatePanelData();
     }
   );
 }
-
-export function generateTutorialPayload() {
+export function generatePanelPayload() {
   return {
-    title: faker.name.title(),
+    name: faker.name.title(),
     description: faker.lorem.text(),
-  };
-}
-//login
-export function generateLoginData(overide = {}) {
-  return {
-    userName: faker.name.title(),
-    password: faker.name.title(),
-    role: faker.lorem.word(),
-    ...overide,
-  };
-}
-export function generateLoginsData(n: number = 1) {
-  return Array.from(
-    {
-      length: n,
-    },
-    (_, i) => {
-      return generateLoginData();
-    }
-  );
-}
-export function generateLoginPayload() {
-  return {
-    userName: faker.name.title(),
-    password: faker.name.title(),
-    role: faker.lorem.word(),
+    tests: [],
   };
 }
