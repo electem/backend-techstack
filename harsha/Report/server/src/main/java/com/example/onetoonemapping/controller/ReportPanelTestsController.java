899df3e6c147d20ba04346755c0488520d350e28
HarshaMC11 <harsha@electems.com>
2022-09-21 19:00:32 +0530

    modified Map collection code on backend

--- harsha/Report/server/src/main/java/com/example/onetoonemapping/controller/ReportPanelTestsController.java
+++ harsha/Report/server/src/main/java/com/example/onetoonemapping/controller/ReportPanelTestsController.java
@@ -1,6 +1,5 @@
 package com.example.onetoonemapping.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,21 +28,10 @@ public class ReportPanelTestsController {
 
 	@GetMapping("/reportPanelTests")
 	public Map<String, String> getReportPanelTest() {
-		List<Object[]> list = reportRepository.getReportPanelTests();
-		List<ReportPanelTests> reportPanelTestLists = new ArrayList<ReportPanelTests>();
-		for (Object[] object : list) {
-			ReportPanelTests report = new ReportPanelTests();
-			report.setId((int) object[0]);
-			report.setData((String) object[1]);
-			report.setReportId((Integer) object[2]);
-			report.setPanelId((Integer) object[3]);
-			report.setTestId((Integer) object[4]);
-			reportPanelTestLists.add(report);
-		}
+		List<Object[]> reportPanelTestLists = reportRepository.getReportPanelTests();
 		Map<String, String> map = new HashMap<String, String>();
-		for (ReportPanelTests reportPanelTest : reportPanelTestLists) {
-			map.put(reportPanelTest.getPanelId() + " " + reportPanelTest.getTestId(),
-					reportPanelTest.getData());
+		for (Object[] object : reportPanelTestLists) {
+			map.put(object[3] + "_" + object[4], (String) object[1]);
 		}
 		return map;
 	}
