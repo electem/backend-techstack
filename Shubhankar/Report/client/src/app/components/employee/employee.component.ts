a641c43798df24d865839c801ea2734b57067d10
shubhankar54 <shubhankar@electems.com>
2022-09-28 17:27:46 +0530

    json from backend display in angular

---
+++ Shubhankar/Report/client/src/app/components/employee/employee.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Company} from 'src/app/models/company';
+import { Employee } from 'src/app/models/employee';
+import { PannelserviceService } from 'src/app/services/pannelservice.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  employee: Employee = {
+    name: '',
+    address: '',
+    gender: '',
+    salary: 1,
+  };
+  companys?: Company[];
+  
+  submitted?: boolean;
+  employees?: Employee[] =[];
+  
+
+  constructor(private PannelserviceService: PannelserviceService) {}
+
+  ngOnInit(): void {
+    this.getEmployee();
+    this.getCompanies();
+    this.getEmployees();
+  }
+
+  async getEmployees(){
+      this.employees  = await this.PannelserviceService.getEmployees()
+  }
+
+  getEmployee() {
+    this.employee = this.PannelserviceService.getEmployee();
+  }
+
+  getCompanies() {
+    this.companys = this.PannelserviceService.getCompany();
+  }
+
+  async  SaveData(){
+    this.submitted = true
+   const employee: Employee = {
+      name:this.employee.name,
+      address: this.employee.address,
+      gender: this.employee.gender,
+      salary: this.employee.salary,
+    };
+    await this.PannelserviceService.createEmployee(employee);
+  }
+}
