b897ba2bd5ff93c1912b388ab3fcf600b8dd8ab4
HarshaMC11 <harsha@electems.com>
2022-09-30 17:05:03 +0530

    Using Map saving panel object - frontend

--- harsha/Report/client/src/app/components/panel-list/panel-list.component.ts
+++ harsha/Report/client/src/app/components/panel-list/panel-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Panel } from 'src/app/models/panel.model';
 import { PanelService } from 'src/app/services/panel.service';
 import { Test } from 'src/app/models/test.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-panel-list',
@@ -10,25 +11,37 @@ import { Test } from 'src/app/models/test.model';
 })
 export class PanelListComponent implements OnInit {
   panels: Panel[] = [];
-  currentIndex = -1;
   tests: Test[] = [];
   selectedTests: Test[] = [];
-  selectedTestNew = new Test();
   selectedTest = new Test();
-  editForm: boolean = false;
   panel: Panel = {
     name: '',
     description: '',
     tests: [],
   };
+
   panelForm?: boolean;
   testForm?: boolean;
-
-  constructor(private panelService: PanelService) {}
+  constructor(
+    private panelService: PanelService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.retrievePanels();
     this.getTests();
+  }
+
+  randomString(length: number) {
+    var randomChars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for (var i = 0; i < length; i++) {
+      result += randomChars.charAt(
+        Math.floor(Math.random() * randomChars.length)
+      );
+    }
+    return result;
   }
 
   async retrievePanels(): Promise<void> {
@@ -43,7 +56,7 @@ export class PanelListComponent implements OnInit {
     if (this.tests) {
       for (let test of this.tests) {
         if (test.id == value.id) {
-          this.selectedTests.push(this.selectedTestNew);
+          this.selectedTests.push(test);
         }
       }
     }
@@ -57,34 +70,16 @@ export class PanelListComponent implements OnInit {
     this.testForm = true;
   }
 
-  editPanel(panel: Panel) {
-    this.panel = panel;
-    this.editForm = true;
-  }
-
   async savePanel(): Promise<void> {
     const panelData: Panel = {
       name: this.panel.name,
       description: this.panel.description,
     };
-    await this.panelService.createPanel(panelData);
-  }
-
-  async updatePanel(): Promise<void> {
-    const panel = {
-      id: this.panel.id,
-      title: this.panel.name,
-      description: this.panel.description,
-      tests: this.selectedTests,
-    };
-    if (this.selectedTests.length) await this.panelService.updatePanel(panel);
+    await this.panelService.createPanelByMap(panelData);
+    this.panelForm = false;
   }
 
   cancelPanel() {
     this.panelForm = false;
-    this.panel = {
-      name: '',
-      description: '',
-    };
   }
 }
--- harsha/Report/client/src/app/services/panel.service.ts
+++ harsha/Report/client/src/app/services/panel.service.ts
@@ -5,17 +5,27 @@ import { ReportPanelTest } from '../models/report-panel-test.model';
 import { Test } from '../models/test.model';
 import { Pagination } from '../models/pagination.model';
 import { Employee } from '../models/employee.model';
+import productJson from '../product.json';
+import productJsonData from '../productList.json';
 
 const baseUrl = 'http://localhost:8080';
+
+interface Product {
+  id?: number;
+  name?: string;
+  type?: string;
+  amount?: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class PanelService {
   tokenIs!: string;
   panel: Panel[] = [];
-  pagination = new Pagination;
+  pagination = new Pagination();
+  productObject = productJson;
+  products: Product[] = productJsonData;
 
   constructor(private http: HttpClient) {}
 
@@ -28,14 +38,15 @@ export class PanelService {
   }
 
   async createPanel(panel: Panel): Promise<Panel> {
-    return await this.http.post(baseUrl + '/createPanel', panel).toPromise();
+    return await this.http
+      .post<Panel>(baseUrl + '/createPanel', panel)
+      .toPromise();
   }
 
   async updatePanel(id: any, panel: Panel): Promise<Panel> {
     return await this.http
       .put(`${baseUrl + '/updatePanel'}/${id}`, panel)
       .toPromise();
-      
   }
   async getTests(): Promise<Test[]> {
     return await this.http.get<Test[]>(baseUrl + '/tests').toPromise();
@@ -54,12 +65,39 @@ export class PanelService {
       .get<ReportPanelTest[]>(baseUrl + '/reportPanelTests')
       .toPromise();
   }
-  
-  async getEmployees(startPoint:number,pageLength:number): Promise<Employee[]>{
-    this.pagination={
-      startPoint:startPoint,
-      pageLength:pageLength
-    }
-    return await this.http.post<Employee[]>(baseUrl + '/employees',this.pagination).toPromise();
+
+  async getEmployees(
+    startPoint: number,
+    pageLength: number
+  ): Promise<Employee[]> {
+    this.pagination = {
+      startPoint: startPoint,
+      pageLength: pageLength,
+    };
+    return await this.http
+      .post<Employee[]>(baseUrl + '/employees', this.pagination)
+      .toPromise();
+  }
+
+  async getProduct() {
+    return await this.productObject;
+  }
+
+  async getProductsList() {
+    return await this.products;
+  }
+
+  async createProduct(product: Product): Promise<any> {
+    console.log(product);
+    return await this.http
+      .post(baseUrl + '/createProduct', product)
+      .toPromise();
+  }
+
+  async createPanelByMap(panel: Panel): Promise<Panel> {
+    const mapObject = { name: panel.name, description: panel.description };
+    return await this.http
+      .post(baseUrl + '/createPanelByMap', mapObject)
+      .toPromise();
   }
 }
