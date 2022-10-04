4e6155d3cec88c235c16c20d7593fe71dba980f7
HarshaMC11 <harsha@electems.com>
2022-10-04 12:22:30 +0530

    modifed files

---
+++ harsha/Report/server/src/main/java/com/example/onetoonemapping/models/Report.java
@@ -0,0 +1,30 @@
+package com.example.onetoonemapping.models;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "reports")
+@Builder
+@Data
+public class Report {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String name;
+
+	@Column(name = "createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private Date createdAt;
+}
---
+++ harsha/Report/server/src/main/java/com/example/onetoonemapping/models/ReportPanelTests.java
@@ -0,0 +1,26 @@
+package com.example.onetoonemapping.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ReportPanelTests {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String data;
+
+	private int reportId;
+
+	private int panelId;
+
+	private int testId;
+
+}
