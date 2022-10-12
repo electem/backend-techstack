4b4f48949f8bdf9c114c56aad48e0080c6b0adc5
shashikiran0620 <shashi@electems.com>
2022-10-12 12:23:56 +0530

    commit

--- shashi/nestJs/backend/src/app.controller.ts
+++ shashi/nestJs/backend/src/app.controller.ts
@@ -1,12 +1,9 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
-<<<<<<< HEAD
+
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
-=======
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
->>>>>>> 29dceecd661f20fa9132e5e255dfa422005be965
 import { AppService } from './app.service';
 
 import { ArticleDto } from './articles/dto/article.dto';
@@ -19,8 +16,6 @@ export class AppController {
   getHello(): ArticleDto[] {
     return this.appService.getHello();
   }
-
-<<<<<<< HEAD
   // @Post()
   // async createArticle(@Body() article: ArticleDto): Promise<ArticleDto> {
   //   return this.appService.createArticle(article);
@@ -30,12 +25,11 @@ export class AppController {
   // async createImage(image: QueryFileDto): Promise<QueryFileDto> {
   //   return this.appService.createImage(image);
   // }
-=======
+
   @Post()
   createArticle(@Body() article: ArticleDto): Promise<ArticleDto> {
     return this.appService.createArticle(article);
   }
->>>>>>> 29dceecd661f20fa9132e5e255dfa422005be965
   // @Get(':id')
   // GetProductById(@Param() param: number) {
   //     return this.article.find(p => p.id === +param.id);
