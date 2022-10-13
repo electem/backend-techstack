835f33e056fdf005e67192b2ad8401822ff72ccb
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-13 10:12:14 +0530

    panel test commmit

--- druthvik/Report/server/src/models/panel.ts
+++ druthvik/Report/server/src/models/panel.ts
@@ -5,25 +5,19 @@ import {
   ManyToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { JsonObject, JsonProperty } from 'typescript-json-serializer';
 import { Test } from './test';
 
 @Entity()
-@JsonObject()
 export class Panel {
   @PrimaryGeneratedColumn()
-  @JsonProperty()
   id!: number;
 
   @Column()
-  @JsonProperty()
   name!: string;
 
   @Column()
-  @JsonProperty()
   description!: string;
 
-  @JsonProperty()
   @ManyToMany(() => Test, (test: Test) => test.panel, {
     cascade: true,
   })
