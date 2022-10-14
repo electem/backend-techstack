85fac465730dec32c057785cab9b92147b3c501e
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-14 16:57:17 +0530

    customer edit

--- druthvik/Customers/Server/src/repositories/customer.repository.ts
+++ druthvik/Customers/Server/src/repositories/customer.repository.ts
@@ -22,3 +22,23 @@ export const getCustomers = async (): Promise<Array<customerModel>> => {
   const customerRepository = getRepository(customerModel);
   return customerRepository.find();
 };
+
+export const getCustomerById = async (
+  id: number,
+): Promise<customerModel | null> => {
+  const customerRepository = getRepository(customerModel);
+  const customer = await customerRepository.findOne({ id: id });
+  if (!customer) return null;
+  return customer;
+};
+
+export const updateCustomer = async (
+  payload: ICustomerPaylod,
+): Promise<ICustomerPaylod> => {
+  const customerRepository = getRepository(customerModel);
+  const customer = new customerModel();
+  return customerRepository.save({
+    ...customer,
+    ...payload,
+  });
+};
--- druthvik/Customers/Server/src/routes/customer.router.ts
+++ druthvik/Customers/Server/src/routes/customer.router.ts
@@ -13,4 +13,18 @@ router.get('/', async (_req, res) => {
   const response = await controller.getCustomers();
   return res.send(response);
 });
+
+router.get('/:id', async (req, res) => {
+  const controller = new customerController();
+  const response = await controller.getCustomerById(req.params.id);
+  if (!response) res.status(404).send({ message: 'No customers found' });
+  return res.send(response);
+});
+
+router.put('/', async (req, res) => {
+  const controller = new customerController();
+  const response = await controller.updateCustomer(req.body);
+  return res.send(response);
+});
+
 export default router;
