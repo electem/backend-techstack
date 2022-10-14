008c10f328cc171c69bd020d34589f581c14821c
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-14 13:07:49 +0530

    customer create backend

--- druthvik/Customers/Server/src/routes/index.ts
+++ druthvik/Customers/Server/src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import registerRouter from './userreister.router';
+import customerRouter from './customer.router';
 
 router.use('/registeruser', registerRouter);
+router.use('/createcustomer', customerRouter);
 
 export default router;
