c0d33de9cae7fb033957e7c2449a2f8039ea741d
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-17 18:16:35 +0530

    commit

--- druthvik/Customers/Client/src/app/models/customer.model.ts
+++ druthvik/Customers/Client/src/app/models/customer.model.ts
@@ -1,7 +1,10 @@
+import { customerGroup } from './customergroup.model';
+
 export class createCustomer {
   id?: number;
   name?: string;
   phonenumber?: number;
   address?: string;
   status?: string;
+  customergroup: customerGroup[];
 }
