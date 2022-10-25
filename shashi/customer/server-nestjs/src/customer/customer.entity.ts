4f2f155b326208a398a945ea3f70081895973ff5
shashikiran0620 <shashi@electems.com>
2022-10-25 10:37:23 +0530

    nestjs backend commit with associations

--- shashi/customer/server-nestjs/src/customer/customer.entity.ts
+++ shashi/customer/server-nestjs/src/customer/customer.entity.ts
@@ -2,10 +2,11 @@
 import { Column, Model, Table, BelongsToMany } from 'sequelize-typescript';
 import { CustomerGroup } from '../customerGroup/customergroup.entity';
 import { customerCustomerGroup } from './customer-customergroup.model';
+import { Unit } from 'src/unit/unit.entity';
+import { customerUnit } from 'src/unit/unit-customer.entity';
 
 @Table
 export class Customer extends Model {
-  
   @Column({
     primaryKey: true,
     autoIncrement: true,
@@ -29,4 +30,7 @@ export class Customer extends Model {
 
   @BelongsToMany(() => CustomerGroup, () => customerCustomerGroup)
   customer: CustomerGroup[];
+
+  @BelongsToMany(() => Unit, () => customerUnit)
+  units: Unit[];
 }
