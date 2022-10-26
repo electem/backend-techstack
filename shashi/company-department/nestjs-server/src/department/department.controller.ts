e6f1c3890d3f841e45a61f307e94f4e57d2b5185
shashikiran0620 <shashi@electems.com>
2022-10-26 12:45:33 +0530

    department listing backend code commit

--- shashi/company-department/nestjs-server/src/department/department.controller.ts
+++ shashi/company-department/nestjs-server/src/department/department.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Department } from './department.entity';
 import { DepartmentService } from './department.service';
 
@@ -10,5 +10,9 @@ export class DepartmentController {
   @Post()
   async createDepartment(@Body() department: Department) {
     return await this.departmentService.createDepartment(department);
+  }
+  @Get()
+  async getDepartments(): Promise<Array<Department>> {
+    return this.departmentService.getDepartments();
   }
 }
