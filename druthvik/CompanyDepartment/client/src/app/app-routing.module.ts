90dc06065da8e4f5a37c8a5add77ab8597af8af2
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-26 13:18:39 +0530

    frontend department list

--- druthvik/CompanyDepartment/client/src/app/app-routing.module.ts
+++ druthvik/CompanyDepartment/client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreatecompanyComponent } from './components/createcompany/createcompany.component';
 import { CompanylistingComponent } from './components/companylisting/companylisting.component';
 import { CreatedepartmentComponent } from './components/createdepartment/createdepartment.component';
-
+import { DepartmentlistingComponent } from './components/departmentlisting/departmentlisting.component';
 const routes: Routes = [
   { path: '', redirectTo: 'createcompany', pathMatch: 'full' },
   { path: 'createcompany', component: CreatecompanyComponent },
   { path: 'comapanylisting', component: CompanylistingComponent },
   { path: 'createdepartment', component: CreatedepartmentComponent },
+  { path: 'departmentlisting', component: DepartmentlistingComponent },
 ];
 
 @NgModule({
