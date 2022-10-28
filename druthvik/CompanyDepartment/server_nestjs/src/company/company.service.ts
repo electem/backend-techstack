f2cb79468c6e5043fb3fd5abf882475c7132f5a8
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-28 16:28:08 +0530

    getbyid update delete for company and department

--- druthvik/CompanyDepartment/server_nestjs/src/company/company.service.ts
+++ druthvik/CompanyDepartment/server_nestjs/src/company/company.service.ts
@@ -13,6 +13,7 @@ export class CompanyService {
   public async findAllCompany(): Promise<Company[]> {
     return await this.companyRepository.find();
   }
+
   public async createCompany(companyDto: CompanyDto): Promise<Company> {
     try {
       return await this.companyRepository.save(companyDto);
@@ -28,5 +29,26 @@ export class CompanyService {
       .where('company.id= :id', { id: id })
       .getOne();
     return postWithQueryBuilder;
+  }
+  async getAllCompanyWithDepartment() {
+    const getAll = await this.companyRepository
+      .createQueryBuilder('company')
+      .select(['company', 'department'])
+      .leftJoinAndSelect('company.department', 'department')
+      .getMany();
+    return getAll;
+  }
+
+  public async updateCompany(companyDto: CompanyDto): Promise<Company> {
+    try {
+      return await this.companyRepository.save(companyDto);
+    } catch (err) {
+      throw new HttpException(err, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  public async deleteCompany(id: number): Promise<void> {
+    const comapany = await this.findCompanyWithDepartmentById(id);
+    await this.companyRepository.remove(comapany);
   }
 }
--- druthvik/CompanyDepartment/server_nestjs/src/department/department.controller.ts
+++ druthvik/CompanyDepartment/server_nestjs/src/department/department.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { DepartmentService } from './department.service';
 import { DepartmentDto } from './department.dto';
 
@@ -8,15 +17,27 @@ export class DepartmentController {
 
   @Get()
   findAll() {
-    return this.departmentService.findAllDepartments();
+    return this.departmentService.getAllDepartmentWithCompany();
   }
   @Post()
-  async create(@Body() companyDto: DepartmentDto) {
-    return await this.departmentService.createDepartment(companyDto);
+  async create(@Body() departmentDto: DepartmentDto) {
+    return await this.departmentService.createDepartment(departmentDto);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.departmentService.findDepartmentWithCompanyById(+id);
+  }
+  @Put('/')
+  async update(@Body() companyDto: DepartmentDto) {
+    return await this.departmentService.updateDepartment(companyDto);
+  }
+  @Delete('/:id')
+  public async deleteCompany(@Param('id') id: string): Promise<void> {
+    const company = this.departmentService.deleteDepartment(+id);
+    if (!company) {
+      throw new NotFoundException('User does not exist!');
+    }
+    return company;
   }
 }
