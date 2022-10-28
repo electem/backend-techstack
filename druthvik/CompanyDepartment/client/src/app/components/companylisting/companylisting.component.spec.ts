097a24b992440b4ccecd4f9417ed69211d983d62
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-10-28 18:35:56 +0530

    jest config to angular

--- druthvik/CompanyDepartment/client/src/app/components/companylisting/companylisting.component.spec.ts
+++ druthvik/CompanyDepartment/client/src/app/components/companylisting/companylisting.component.spec.ts
@@ -1,16 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompanylistingComponent } from './companylisting.component';
+import { CompanyService } from 'src/app/services/company.service';
 
 describe('CompanylistingComponent', () => {
   let component: CompanylistingComponent;
   let fixture: ComponentFixture<CompanylistingComponent>;
+  let companyservice: CompanyService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompanylistingComponent ]
-    })
-    .compileComponents();
+      declarations: [CompanylistingComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,9 @@ describe('CompanylistingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(component.companies.length).toEqual(1);
   });
 });
