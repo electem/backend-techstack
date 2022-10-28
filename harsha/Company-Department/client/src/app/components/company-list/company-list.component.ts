b992fe25c12a70a49d2148fdf41cc7ea02febbdc
HarshaMC11 <harsha@electems.com>
2022-10-28 22:25:27 +0530

    commit on company-listing

---
+++ harsha/Company-Department/client/src/app/components/company-list/company-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/models/company.model';
+import { CompanyService } from 'src/app/services/company.service';
+
+@Component({
+  selector: 'app-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrls: ['./company-list.component.css'],
+})
+export class CompanyListComponent implements OnInit {
+  companies: Company[] = [];
+  constructor(private companyService: CompanyService) {}
+
+  ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  async getCustomers(): Promise<void> {
+    this.companies = await this.companyService.getCompanies();
+  }
+}
