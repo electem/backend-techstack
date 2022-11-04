020c42a627a847ccb7d1ac614d7cacee21379322
shubhankar54 <shubhankar@electems.com>
2022-11-04 15:31:01 +0530

    hardcoded chart

--- Shubhankar/CompanyDept/client/src/app/components/chart/chart.component.ts
+++ Shubhankar/CompanyDept/client/src/app/components/chart/chart.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
-import { Company } from '../../models/company';
-import { Department } from '../../models/department';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -10,27 +8,9 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./chart.component.css'],
 })
 export class ChartComponent implements OnInit {
-  companys: Company[] = [];
-  companyNames: string[] = [];
-  departmentCount: number[] = [];
-  departments: Department[] = [];
-  departmentNames: string[] = [];
-  companyCounts: number[] = [];
-
   constructor(private userservice: UserService) {}
 
   ngOnInit(): void {
-    this.retrieveCompany();
-    this.retrieveDepartment();
-    this.callMixedChart();
-  }
-
-  async retrieveCompany() {
-    this.companys = await this.userservice.getCompanys();
-    for (var compnay of this.companys) {
-      this.companyNames.push(compnay.name!);
-      this.departmentCount.push(compnay.department?.length!);
-    }
     this.callBarChart();
   }
 
@@ -39,89 +19,16 @@ export class ChartComponent implements OnInit {
     const myCharts = new Chart('myBarChart', {
       type: 'bar',
       data: {
-        labels: this.companyNames,
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [
           {
-            label: '# Number of Departments present for each compnay',
-            data: this.departmentCount,
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: ['rgba(54, 162, 235, 0.2)'],
             borderColor: ['rgba(54, 162, 235, 1)'],
             borderWidth: 1,
           },
         ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
-  }
-
-  public callLineChart() {
-    Chart.register(...registerables);
-    const myChart = new Chart('myLineChart', {
-      type: 'line',
-      data: {
-        labels: this.departmentNames,
-        datasets: [
-          {
-            label: '# Number of Companys present for each Department',
-            data: this.companyCounts,
-            backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-            borderColor: ['rgba(54, 162, 235, 1)'],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
-  }
-
-  async retrieveDepartment() {
-    this.departments = await this.userservice.getDepartments();
-    for (var department of this.departments) {
-      this.departmentNames.push(department.name!);
-      console.log(this.departmentNames);
-      this.companyCounts.push(department.company?.length!);
-      console.log(this.companyCounts);
-    }
-    this.callLineChart();
-  }
-
-  public callMixedChart() {
-    Chart.register(...registerables);
-    const mixedChart = new Chart('myMixedChart', {
-      data: {
-        datasets: [
-          {
-            type: 'bar',
-            label:
-              'Bar Dataset # Number of Departments present in each company',
-            data: this.departmentCount,
-            backgroundColor: ['rgba(59, 162, 235, 0.2)'],
-            borderColor: ['rgba(54, 162, 235, 1)'],
-            borderWidth: 1,
-          },
-          {
-            type: 'bar',
-            label:
-              'Line Dataset # Number of Companys present in each department',
-            data: this.companyCounts,
-            backgroundColor: ['rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgba(54, 162, 235, 1)'],
-            borderWidth: 1,
-          },
-        ],
-        labels: this.departmentNames,
       },
       options: {
         scales: {
