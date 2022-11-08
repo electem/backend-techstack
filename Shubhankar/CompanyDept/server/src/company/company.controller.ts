5ebbf455eaa9fd6764a9c9bd296fe7e2dcf240d7
shubhankar54 <shubhankar@electems.com>
2022-11-08 19:05:06 +0530

    getbyid

--- Shubhankar/CompanyDept/server/src/company/company.controller.ts
+++ Shubhankar/CompanyDept/server/src/company/company.controller.ts
@@ -33,6 +33,44 @@ export class CompanyController {
        return response 
   }  
   
+  @Post('pdfemail/:id')
+  async postHTMLEmailId(@Param('id') id: string) {
+    const companybyid = await this.companyService.getCompanyById(
+      +id,
+    );
+    const compile = async function (templatename, data) {
+      const html = fs.readFileSync(
+        join(__dirname, '../../src/mails/email.hbs'),
+        'utf-8',
+      );
+      return hbs.compile(html)(companybyid);
+    };
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+    const content = compile('company', companybyid);
+    console.log(content);
+    await page.setContent(await content);
+    await page.pdf({
+      format: 'A4',
+      path: join(__dirname, '../../src/mails/email.pdf'),
+    });
+    const response = await this.mailService.sendMail({
+      to: companybyid.email,
+      from: 'shubhankar@electems.com',
+      subject: 'send email with attachment',
+      attachments: [
+        {
+          path: join(__dirname, '../../src/mails/company.pdf'),
+          filename: 'document.pdf',
+          contentDisposition: 'attachment',
+        },
+      ],
+    });
+    return response;
+  }
+
   @Post('/pdfemail')
   async postHTMLEmail(@Body() company: Company[]){
     const companies = await this.companyService.getAllCompanyDepartment();
@@ -95,7 +133,7 @@ export class CompanyController {
   async updateCompany(@Body() companyDto: CompanyDto) {
     return await this.companyService.updateCompany(companyDto);
   }
-
+  
   @Delete('/:id')
   public async deleteCompany(@Param('id') id: string): Promise<void> {
     const company = this.companyService.deleteCompanyById(+id);
