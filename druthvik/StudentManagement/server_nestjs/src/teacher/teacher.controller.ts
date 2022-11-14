2522c02f5f0b0e912400cbbbda50669cf5df9602
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-11-14 15:40:08 +0530

    mail send backend

--- druthvik/StudentManagement/server_nestjs/src/teacher/teacher.controller.ts
+++ druthvik/StudentManagement/server_nestjs/src/teacher/teacher.controller.ts
@@ -12,12 +12,19 @@ import {
 } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { TeacherDto } from './teacher.dto';
-
+import { join } from 'path';
+import * as fs from 'fs';
+import hbs from 'handlebars';
+import puppeteer from 'puppeteer';
+import { MailerService } from '@nestjs-modules/mailer';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('teacher')
 export class TeacherController {
-  constructor(private teacherService: TeacherService) {}
+  constructor(
+    private teacherService: TeacherService,
+    private mailService: MailerService,
+  ) {}
 
   @Get()
   findAll() {
@@ -26,7 +33,38 @@ export class TeacherController {
 
   @Post()
   async create(@Body() teacherDto: TeacherDto) {
-    return await this.teacherService.createTeacher(teacherDto);
+    const teacher = await this.teacherService.createTeacher(teacherDto);
+    const compile = async function (templatename, data) {
+      const html = fs.readFileSync(
+        join(__dirname, '../../src/mails/teacher.hbs'),
+        'utf-8',
+      );
+      return hbs.compile(html)(data);
+    };
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+    const content = compile('teacher', teacher);
+    console.log(content);
+    await page.setContent(await content);
+    await page.pdf({
+      format: 'A4',
+      path: join(__dirname, '../../src/mails/teacher.pdf'),
+    });
+    const response = await this.mailService.sendMail({
+      to: teacher.email,
+      from: 'druthvik@electems.com',
+      subject: 'Teacher Information',
+      attachments: [
+        {
+          path: join(__dirname, '../../src/mails/teacher.pdf'),
+          filename: `${teacher.name}.pdf`,
+          contentDisposition: 'attachment',
+        },
+      ],
+    });
+    return teacher;
   }
 
   //@UseGuards(JwtAuthGuard)
