ce99aee2ef5fc8aa66b9e9f0b60715dcfa835acf
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2022-11-14 15:39:36 +0530

    front end school binding fix update and create button fix file upload added

--- druthvik/StudentManagement/client/src/app/components/createstudent/createstudent.component.ts
+++ druthvik/StudentManagement/client/src/app/components/createstudent/createstudent.component.ts
@@ -28,21 +28,19 @@ export class CreatestudentComponent implements OnInit {
     phonenumber: null,
     email: '',
     gender: '',
-    school: {},
+    school: new School(),
   };
   schools: School[];
-  genders: Gender[];
+  genders: string[];
   date = new Date();
-  options: DatepickerOptions = {
-    format: 'DD-MM-YYYY',
-    placeholder: 'select date',
-    calendarClass: 'datepicker-default',
-  };
-  selectedSchool = new School();
+  currentSchool = new School();
   selectedGender: string;
   file: File;
   editFile = true;
   removeUpload = false;
+  downloadfile: any;
+  id: number;
+  addForm: boolean;
   constructor(
     private studentservice: StudentService,
     private formBuilder: FormBuilder,
@@ -58,15 +56,21 @@ export class CreatestudentComponent implements OnInit {
     this.createtStudentForm = this.formBuilder.group({
       name: ['', Validators.required],
       address: ['', Validators.required],
-      phonenumber: ['', Validators.required],
-      email: ['', Validators.required],
+      phonenumber: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]],
       gender: ['', Validators.required],
       school: ['', Validators.required],
       dateofbirth: ['', Validators.required],
+      file: ['', Validators.required],
     });
+
     this.retrieveSchools();
     this.getGenders();
-    this.getStudentById(this.route.snapshot.params.id);
+    if (this.route.snapshot.params.id) {
+      this.getStudentById(this.route.snapshot.params.id);
+    }
+    this.id = this.route.snapshot.params.id;
+    this.addForm = !this.id;
   }
   get f() {
     return this.createtStudentForm.controls;
@@ -85,9 +89,10 @@ export class CreatestudentComponent implements OnInit {
       address: this.createstudent.address,
       phonenumber: this.createstudent.phonenumber,
       email: this.createstudent.email,
-      gender: this.selectedGender,
+      gender: this.createstudent.gender,
       dateofbirth: this.createstudent.dateofbirth,
       school: this.createstudent.school,
+      file: this.createstudent.file,
     };
     await this.studentservice.createStudent(createStudents);
     this.router.navigate(['/liststudents']);
@@ -99,29 +104,26 @@ export class CreatestudentComponent implements OnInit {
   getGenders() {
     this.genders = this.teacherservice.getGenders();
   }
-  onSelected(school: School) {
-    this.createstudent.school = school;
-  }
 
   getSelectedGenderChange(gender: Gender) {
     this.selectedGender = gender.name;
   }
-  onStudentGenderChange(gender: Gender) {
-    this.getSelectedGenderChange(gender);
-  }
+
   async getStudentById(id: number) {
     this.createstudent = await this.studentservice.getStudentById(id);
   }
 
   async updateStudent(): Promise<void> {
     const student: Student = {
-      id: this.createstudent.id,
+      studentid: this.createstudent.studentid,
       name: this.createstudent.name,
+      address: this.createstudent.address,
       phonenumber: this.createstudent.phonenumber,
       email: this.createstudent.email,
-      gender: this.createstudent.gender,
+      gender: this.selectedGender,
       dateofbirth: this.createstudent.dateofbirth,
       school: this.createstudent.school,
+      file: this.createstudent.file,
     };
     await this.studentservice.updateStudent(student);
     this.router.navigate(['/liststudents']);
@@ -144,5 +146,25 @@ export class CreatestudentComponent implements OnInit {
       };
       this.cd.markForCheck();
     }
+  }
+  async onSelectedSchool(school: string) {
+    console.log(school);
+    const data = this.schools.filter((s) => s.schoolid === +school);
+    this.currentSchool = data[0];
+  }
+  async downloadFile() {
+    this.downloadfile = await this.fileService
+      .downloadFile(this.file)
+      .subscribe((response) => {
+        const filename = response.headers
+          .get('content-disposition')
+          ?.split(';')[1]
+          .split('=')[1];
+        const blob: Blob = response.body as Blob;
+        const a = document.createElement('a');
+        a.download = filename!;
+        a.href = window.URL.createObjectURL(blob);
+        a.click();
+      });
   }
 }
