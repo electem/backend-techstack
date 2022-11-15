ed0d93685eb9d8e6a4d0b55d65509ff331373f58
shubhankar54 <shubhankar@electems.com>
2022-11-15 19:00:29 +0530

    school list vuejs

---
+++ Shubhankar/School/client vuejs/src/services/SchoolService.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import http from "@/http-common";
+
+
+class SchoolService {
+
+    getAllSchool(): Promise<any> {
+        return http.get("/school");
+      }  
+}
+
+export default new SchoolService();
