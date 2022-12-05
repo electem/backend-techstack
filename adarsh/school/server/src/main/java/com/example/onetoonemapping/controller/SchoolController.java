65209b24b920e10cd5d953188ca1a122b32c527f
HarshaMC11 <109588624+HarshaMC11@users.noreply.github.com>
2022-12-05 16:02:03 +0530

    Update SchoolController.java
    API Integration.

--- adarsh/school/server/src/main/java/com/example/onetoonemapping/controller/SchoolController.java
+++ adarsh/school/server/src/main/java/com/example/onetoonemapping/controller/SchoolController.java
@@ -57,5 +57,31 @@ private Logger log = LoggerFactory.getLogger(SchoolController.class);
 		log.info("Start of StudentController :: updateStudent ");
 		return schoolRepo.save(school);
 	}
+	
+	@PostMapping("/client/post")
+    	private void sendPOST(@Valid @RequestBody School school) throws IOException {
+        log.info("Start of StudentController :: sendPOST() ");
+        String url = "http://localhost:8080/addSchools";
+        String result = "";
+        HttpPost post = new HttpPost(url);
+        post.setHeader("Content-Type", "application/json;charset=UTF-8");
+        post.setHeader("Accept", "application/json, application/json");
+        Teachers teachers = new Teachers();
+        for (Teachers teacher : school.getTeachers()) {
+            teachers.setEmail(teacher.getEmail());
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+        json = objectMapper.writeValueAsString(teachers);
+        post.setEntity(new StringEntity(json.toString()));
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+                CloseableHttpResponse response = httpClient.execute(post)) {
+            result = EntityUtils.toString(response.getEntity());
+
+        }
+        String fileName = "E:\\emailproject\\adarsh" + (int) (Math.random() * 100000);
+        String htmlStr = schoolService.strToHtmlFile(result, fileName + ".html");
+        boolean result1 = schoolService.convert(htmlStr, fileName + ".pdf");
+    }
 
 }
