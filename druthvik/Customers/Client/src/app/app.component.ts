4297cfb0507a1e25ec42e83cfe2373f9e7f029e1
Druthvik7 <107387477+Druthvik7@users.noreply.github.com>
2023-09-14 19:04:26 +0530

    Angular Animation

--- druthvik/Customers/Client/src/app/app.component.ts
+++ druthvik/Customers/Client/src/app/app.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { VERSION } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  animateChild
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('false', style({ height: '*' })),
+      state('true', style({ height: '0px' })),
+      transition('false <=> true', [animate(500)])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'Angular Crud';
+  name = 'Angular ' + VERSION.major;
+  show = true;
+
+  toggle() {
+    this.show = !this.show
+  };
+
+
 }
